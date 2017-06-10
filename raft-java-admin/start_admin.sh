2ddab5f7740db73569b85f4302b45a3217b07fb2
wenweihu86 <wenweihu86@163.com>
2017-06-10 23:37:39 +0800

    update rpc-java to 1.5.0

--- raft-java-admin/start_admin.sh
+++ raft-java-admin/start_admin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-java -cp dependency/*:raft-java-admin-1.1.0.jar com.github.wenweihu86.raft.admin.AdminMain "127.0.0.1:8051,127.0.0.1:8052,127.0.0.1:8053" conf get
+java -cp dependency/*:raft-java-admin-1.2.0.jar com.github.wenweihu86.raft.admin.AdminMain "127.0.0.1:8051,127.0.0.1:8052,127.0.0.1:8053" conf get
 
-java -cp dependency/*:raft-java-admin-1.1.0.jar com.github.wenweihu86.raft.admin.AdminMain "127.0.0.1:8051,127.0.0.1:8052,127.0.0.1:8053" conf add "127.0.0.1:8054:4,127.0.0.1:8055:5"
+java -cp dependency/*:raft-java-admin-1.2.0.jar com.github.wenweihu86.raft.admin.AdminMain "127.0.0.1:8051,127.0.0.1:8052,127.0.0.1:8053" conf add "127.0.0.1:8054:4,127.0.0.1:8055:5"
 
-java -cp dependency/*:raft-java-admin-1.1.0.jar com.github.wenweihu86.raft.admin.AdminMain "127.0.0.1:8051,127.0.0.1:8052,127.0.0.1:8053" conf del "127.0.0.1:8054:4,127.0.0.1:8055:5"
+java -cp dependency/*:raft-java-admin-1.2.0.jar com.github.wenweihu86.raft.admin.AdminMain "127.0.0.1:8051,127.0.0.1:8052,127.0.0.1:8053" conf del "127.0.0.1:8054:4,127.0.0.1:8055:5"
--- raft-java-example/start_client.sh
+++ raft-java-example/start_client.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-java -cp dependency/*:raft-java-example-1.1.0.jar com.github.wenweihu86.raft.example.client.ClientMain "127.0.0.1:8051,127.0.0.1:8052,127.0.0.1:8053" hello raft
+java -cp dependency/*:raft-java-example-1.2.0.jar com.github.wenweihu86.raft.example.client.ClientMain "127.0.0.1:8051,127.0.0.1:8052,127.0.0.1:8053" hello raft
 
-java -cp dependency/*:raft-java-example-1.1.0.jar com.github.wenweihu86.raft.example.client.ConcurrentClientMain "127.0.0.1:8051,127.0.0.1:8052,127.0.0.1:8053"
+java -cp dependency/*:raft-java-example-1.2.0.jar com.github.wenweihu86.raft.example.client.ConcurrentClientMain "127.0.0.1:8051,127.0.0.1:8052,127.0.0.1:8053"
