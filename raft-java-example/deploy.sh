fd407924f9dfd77c85e4a36a763f9f0f3d681f36
wenweihu86 <wenweihu86@163.com>
2018-07-04 17:51:21 +0800

    修复example打包部署错误

--- raft-java-example/deploy.sh
+++ raft-java-example/deploy.sh
@@ -2,31 +2,31 @@
 
 mvn clean package
 
-EXAMPLE_ZIP=raft-java-example-1.8.0-deploy.zip
+EXAMPLE_TAR=raft-java-example-1.8.0-deploy.tar.gz
 ROOT_DIR=./env
 mkdir -p $ROOT_DIR
 cd $ROOT_DIR
 
 mkdir example1
 cd example1
-cp -f ../../target/$EXAMPLE_ZIP .
-tar -zxvf $EXAMPLE_ZIP
+cp -f ../../target/$EXAMPLE_TAR .
+tar -zxvf $EXAMPLE_TAR
 chmod +x ./bin/*.sh
 nohup ./bin/run_server.sh ./data "127.0.0.1:8051:1,127.0.0.1:8052:2,127.0.0.1:8053:3" "127.0.0.1:8051:1" &
 cd -
 
 mkdir example2
 cd example2
-cp -f ../../target/$EXAMPLE_ZIP .
-tar -zxvf $EXAMPLE_ZIP
+cp -f ../../target/$EXAMPLE_TAR .
+tar -zxvf $EXAMPLE_TAR
 chmod +x ./bin/*.sh
 nohup ./bin/run_server.sh ./data "127.0.0.1:8051:1,127.0.0.1:8052:2,127.0.0.1:8053:3" "127.0.0.1:8052:2" &
 cd -
 
 mkdir example3
 cd example3
-cp -f ../../target/$EXAMPLE_ZIP .
-tar -zxvf $EXAMPLE_ZIP
+cp -f ../../target/$EXAMPLE_TAR .
+tar -zxvf $EXAMPLE_TAR
 chmod +x ./bin/*.sh
 nohup ./bin/run_server.sh ./data "127.0.0.1:8051:1,127.0.0.1:8052:2,127.0.0.1:8053:3" "127.0.0.1:8053:3" &
 cd -
