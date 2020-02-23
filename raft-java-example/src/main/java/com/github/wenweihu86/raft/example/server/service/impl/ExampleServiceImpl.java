6686c863a25b9eaeb707e6eb61e2cea020f96504
wenweihu86 <wenweihu86@163.com>
2020-02-23 22:45:00 +0800

    bugfix: RpcException: serviceInterface must not be set repeatedly

--- raft-java-example/src/main/java/com/github/wenweihu86/raft/example/server/service/impl/ExampleServiceImpl.java
+++ raft-java-example/src/main/java/com/github/wenweihu86/raft/example/server/service/impl/ExampleServiceImpl.java
@@ -2,6 +2,9 @@ package com.github.wenweihu86.raft.example.server.service.impl;
 
 import com.baidu.brpc.client.BrpcProxy;
 import com.baidu.brpc.client.RpcClient;
+import com.baidu.brpc.client.RpcClientOptions;
+import com.baidu.brpc.client.instance.Endpoint;
+import com.github.wenweihu86.raft.Peer;
 import com.github.wenweihu86.raft.example.server.ExampleStateMachine;
 import com.github.wenweihu86.raft.example.server.service.ExampleProto;
 import com.github.wenweihu86.raft.example.server.service.ExampleService;
@@ -10,6 +13,9 @@ import com.github.wenweihu86.raft.proto.RaftProto;
 import com.googlecode.protobuf.format.JsonFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by wenweihu86 on 2017/5/9.
@@ -21,10 +27,35 @@ public class ExampleServiceImpl implements ExampleService {
 
     private RaftNode raftNode;
     private ExampleStateMachine stateMachine;
+    private int leaderId = -1;
+    private RpcClient leaderRpcClient = null;
+    private Lock leaderLock = new ReentrantLock();
 
     public ExampleServiceImpl(RaftNode raftNode, ExampleStateMachine stateMachine) {
         this.raftNode = raftNode;
         this.stateMachine = stateMachine;
+        onLeaderChangeEvent();
+    }
+
+    private void onLeaderChangeEvent() {
+        if (raftNode.getLeaderId() != -1
+                && raftNode.getLeaderId() != raftNode.getLocalServer().getServerId()
+                && leaderId != raftNode.getLeaderId()) {
+            leaderLock.lock();
+            if (leaderId != -1 && leaderRpcClient != null) {
+                leaderRpcClient.stop();
+                leaderRpcClient = null;
+                leaderId = -1;
+            }
+            leaderId = raftNode.getLeaderId();
+            Peer peer = raftNode.getPeerMap().get(leaderId);
+            Endpoint endpoint = new Endpoint(peer.getServer().getEndpoint().getHost(),
+                    peer.getServer().getEndpoint().getPort());
+            RpcClientOptions rpcClientOptions = new RpcClientOptions();
+            rpcClientOptions.setGlobalThreadPoolSharing(true);
+            leaderRpcClient = new RpcClient(endpoint, rpcClientOptions);
+            leaderLock.unlock();
+        }
     }
 
     @Override
@@ -34,8 +65,8 @@ public class ExampleServiceImpl implements ExampleService {
         if (raftNode.getLeaderId() <= 0) {
             responseBuilder.setSuccess(false);
         } else if (raftNode.getLeaderId() != raftNode.getLocalServer().getServerId()) {
-            RpcClient rpcClient = raftNode.getPeerMap().get(raftNode.getLeaderId()).getRpcClient();
-            ExampleService exampleService = BrpcProxy.getProxy(rpcClient, ExampleService.class);
+            onLeaderChangeEvent();
+            ExampleService exampleService = BrpcProxy.getProxy(leaderRpcClient, ExampleService.class);
             ExampleProto.SetResponse responseFromLeader = exampleService.set(request);
             responseBuilder.mergeFrom(responseFromLeader);
         } else {
